4a5ae86a786aa9447fef182849327c172c354f00
mrserjo <mrserjo@mail.ru>
2022-04-14 15:33:15 +0300

    APIVIew class

--- BlogComment/comments/views.py
+++ BlogComment/comments/views.py
@@ -2,8 +2,24 @@ from rest_framework import generics
 from django.shortcuts import render
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.forms import model_to_dict
 
 
-class ArticleAPIView(generics.ListAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+class ArticleAPIView(APIView):
+    def get(self, request):
+        lst = Article.objects.all().values()
+        return Response({'lst': lst})
+
+    def post(self, request):
+        post_new = Article.objects.create(
+            title=request.data['title'],
+            content=request.data['content']
+        )
+        return Response({'post': model_to_dict(post_new)})
+
+
+# class ArticleAPIView(generics.ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
